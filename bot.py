a3d3c93c19ec02c5780f43bd031be4baf57f5802
Husniddin <uzbekfreedom@gmail.com>
2023-11-09 11:17:11 +0500

    Base template

--- bot.py
+++ bot.py
@@ -13,8 +13,7 @@ logger = logging.getLogger(__name__)
 async def main():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(filename)s:%(lineno)d #%(levelname)-8s "
-        "[%(asctime)s] - %(name)s - %(message)s",
+       
     )
 
     logger.info("Starting bot")
