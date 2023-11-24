19f53e5d90c81b6eb51dc4bffe59ae8e2ced5c08
Husniddin <uzbekfreedom@gmail.com>
2023-11-24 09:35:16 +0500

    tayyor

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
+from aiogram import  Dispatcher
 
-from config import Config, load_config
-from src.handlers import echo
-
+from handlers import echo
+from loader import bot
 
 logger = logging.getLogger(__name__)
 
@@ -18,9 +17,7 @@ async def main():
 
     logger.info("Starting bot")
 
-    config: Config = load_config()
-
-    bot: Bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+  
     dp: Dispatcher = Dispatcher()
 
     dp.include_router(echo.router)
