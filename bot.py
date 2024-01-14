33ff742ed80ee8ac51cf2222a08642b3c69fb878
Husniddin <103229084+mehmonov@users.noreply.github.com>
2024-01-14 08:33:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,7 @@ import logging
 from aiogram import  Dispatcher
 
 from handlers.echo import router
-from handlers.start import start_router
-from handlers.sign_up import signup_router
-from handlers.menu import menu_router
+
 from loader import bot,  db
 
 logger = logging.getLogger(__name__)
@@ -23,16 +21,11 @@ async def main():
 
     dp: Dispatcher = Dispatcher()
 
-    dp.include_routers(
-        menu_router,
-        signup_router, 
-        start_router, 
+    dp.include_routers( 
         router
     )
 
-    await db.create()
-    await db.create_table_users()
-    await db.create_table_products()
+
 
         
     await bot.delete_webhook(drop_pending_updates=True)
